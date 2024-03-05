00626e38dc298dc87e686fe0297c2ff57fa51c91
Dumebi35 <71632162+Dumi35@users.noreply.github.com>
2024-03-05 02:06:07 +0100

    redirect working, copy minilink feature

--- miniLinkServer/index.js
+++ miniLinkServer/index.js
@@ -26,34 +26,47 @@ const item = require("./models/item"); // Create the Item model
 
 const clientLink = `http://localhost:3000`
 
-// route to handle loading of original urls
-app.use((req, res) => {
- 
-  // Check if the requested URL is not home page
-  if (req.url !== '/') {
-    // const items = item.find({ miniLink: req.query.miniLink }).then(result => res.send(result)).catch(error => console.log(error))
-    console.log("client link "+clientLink+req.url)
-    const items = item.find({ miniLink: clientLink+req.url}).then(result => {console.log("use "+result);res.send(result)}).catch(error => console.log("error "+error))
-  }
-  //next()
-});
-
-//search URL in db to avoid duplicates
-app.get("/", (req, res) => {
-  const items = item.find({ miniLink: req.query.miniLink }).then(result => {console.log("get " + result);res.send(result)}).catch(error => console.log(error))
-})
-
 // Body parsing middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-//sending long URL to db
+// Search URL in db to avoid duplicates
+app.get("/", (req, res) => {
+  const { miniLink } = req.query;
+  item.find({ miniLink })
+    .then(result => res.send(result))
+    .catch(error => console.log(error));
+});
+
+// Sending long URL to db
 app.post("/", (req, res) => {
   const { longURL, miniLink } = req.body;
-  const items = new item({ longURL: longURL, miniLink: miniLink })
-  items.save().then(console.log("record saved")).catch(err => console.log(err))
-})
+  const newItem = new item({ longURL, miniLink });
+  newItem.save()
+    .then(() => {
+      console.log("Record saved");
+      res.send("Record saved"); // Respond to the client after saving
+    })
+    .catch(err => console.log(err));
+});
+
+// Route to handle loading of original urls
+app.get("*", (req, res) => {
+  console.log("Requested URL: " + req.url);
+  // Check if the requested URL is not the home page
+  if (req.url !== '/') {
+    const { url } = req;
+    const miniLink = clientLink + url;
+    item.find({ miniLink })
+      .then(result => {
+        console.log(result);
+        res.send(result);
+      })
+      .catch(error => console.log("error " + error));
+  }
+});
+
 
 
 
